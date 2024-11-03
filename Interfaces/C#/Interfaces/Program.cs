d94b8f35f479a5d7a1ce5a6d6f67705d93e951e3
Raj Huidrom <129941875+Yourboi17@users.noreply.github.com>
2024-11-03 22:52:51 +0530

    Add files via upload

---
+++ Interfaces/C#/Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface IAnimal
+{
+    void Speak();
+}
+
+interface IMammal : IAnimal
+{
+    void Walk();
+}
+
+class Dog : IMammal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Woof!");
+    }
+
+    public void Walk()
+    {
+        Console.WriteLine("The dog walks on four legs.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IMammal myDog = new Dog();
+        myDog.Speak();
+        myDog.Walk();
+
+        Console.ReadLine();
+    }
+}
---
+++ Interfaces/VB/Interfaces/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+    Interface IAnimal
+        Sub Speak()
+    End Interface
+
+    Interface IMammal
+        Inherits IAnimal
+        Sub Walk()
+    End Interface
+
+    Class Dog
+        Implements IMammal
+
+        Public Sub Speak() Implements IAnimal.Speak
+            Console.WriteLine("Woof!")
+        End Sub
+
+        Public Sub Walk() Implements IMammal.Walk
+            Console.WriteLine("The dog walks on four legs.")
+        End Sub
+    End Class
+
+    Sub Main()
+        Dim myDog As IMammal = New Dog()
+        myDog.Speak()
+        myDog.Walk()
+
+        Console.ReadLine()
+    End Sub
+End Module

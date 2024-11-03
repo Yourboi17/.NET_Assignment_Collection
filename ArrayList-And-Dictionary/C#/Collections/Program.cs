ebccf6ac20cff5f6e8e44e096613564c41e93af3
Raj Huidrom <129941875+Yourboi17@users.noreply.github.com>
2024-11-03 23:00:13 +0530

    Add files via upload

---
+++ ArrayList-And-Dictionary/C#/Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add("Apple");
+        arrayList.Add("Banana");
+        arrayList.Add("Cherry");
+
+        Console.WriteLine("ArrayList contents:");
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+        dictionary.Add("Apple", 1);
+        dictionary.Add("Banana", 2);
+        dictionary.Add("Cherry", 3);
+
+        Console.WriteLine("\nDictionary contents:");
+        foreach (var kvp in dictionary)
+        {
+            Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ ArrayList-And-Dictionary/VB/Collections/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+    Sub Main()
+        Dim arrayList As New ArrayList()
+        arrayList.Add("Apple")
+        arrayList.Add("Banana")
+        arrayList.Add("Cherry")
+
+        Console.WriteLine("ArrayList contents:")
+        For Each item In arrayList
+            Console.WriteLine(item)
+        Next
+
+        Dim dictionary As New Dictionary(Of String, Integer)()
+        dictionary.Add("Apple", 1)
+        dictionary.Add("Banana", 2)
+        dictionary.Add("Cherry", 3)
+
+        Console.WriteLine(vbCrLf & "Dictionary contents:")
+        For Each kvp In dictionary
+            Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}")
+        Next
+
+        Console.ReadLine()
+    End Sub
+End Module

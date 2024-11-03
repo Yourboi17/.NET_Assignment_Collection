521c0fd0b1fed63989a656f4eb012a904fba0605
Raj Huidrom <129941875+Yourboi17@users.noreply.github.com>
2024-11-03 22:24:55 +0530

    Add files via upload

---
+++ Basic-Calculator/SimpleCalculator/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.btnPower = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblOperation = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.txtNum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNum1.Location = New System.Drawing.Point(98, 42)
+        Me.txtNum1.Multiline = True
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(87, 40)
+        Me.txtNum1.TabIndex = 0
+        Me.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNum2
+        '
+        Me.txtNum2.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.txtNum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNum2.Location = New System.Drawing.Point(191, 42)
+        Me.txtNum2.Multiline = True
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(87, 40)
+        Me.txtNum2.TabIndex = 1
+        Me.txtNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Mulish", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(179, 103)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(90, 31)
+        Me.lblResult.TabIndex = 2
+        Me.lblResult.Text = "Result:"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.LightGreen
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(422, 194)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 40)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.BackColor = System.Drawing.Color.LightCoral
+        Me.btnSubtract.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubtract.Location = New System.Drawing.Point(98, 193)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(75, 40)
+        Me.btnSubtract.TabIndex = 4
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = False
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.BackColor = System.Drawing.Color.LightGoldenrodYellow
+        Me.btnMultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiply.Location = New System.Drawing.Point(179, 193)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(75, 40)
+        Me.btnMultiply.TabIndex = 5
+        Me.btnMultiply.Text = "*"
+        Me.btnMultiply.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnMultiply.UseVisualStyleBackColor = False
+        '
+        'btnDivide
+        '
+        Me.btnDivide.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btnDivide.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDivide.Location = New System.Drawing.Point(260, 194)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(75, 40)
+        Me.btnDivide.TabIndex = 6
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = False
+        '
+        'btnPower
+        '
+        Me.btnPower.BackColor = System.Drawing.Color.PeachPuff
+        Me.btnPower.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPower.Location = New System.Drawing.Point(341, 194)
+        Me.btnPower.Name = "btnPower"
+        Me.btnPower.Size = New System.Drawing.Size(75, 40)
+        Me.btnPower.TabIndex = 7
+        Me.btnPower.Text = "^"
+        Me.btnPower.UseVisualStyleBackColor = False
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.DarkOrange
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(98, 95)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 43)
+        Me.btnCalculate.TabIndex = 8
+        Me.btnCalculate.Text = "="
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.RosyBrown
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(98, 144)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 43)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'lblOperation
+        '
+        Me.lblOperation.AutoSize = True
+        Me.lblOperation.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOperation.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOperation.Location = New System.Drawing.Point(284, 51)
+        Me.lblOperation.Name = "lblOperation"
+        Me.lblOperation.Size = New System.Drawing.Size(99, 22)
+        Me.lblOperation.TabIndex = 10
+        Me.lblOperation.Text = "Operation:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblOperation)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnPower)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "Form1"
+        Me.Text = "Operation:"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnPower As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblOperation As Label
+End Class
---
+++ Basic-Calculator/SimpleCalculator/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+    Dim result As Double = 0
+    Dim operation As String = ""
+
+    ' This label will display the selected operation
+    Private Sub UpdateOperationLabel()
+        lblOperation.Text = "Operation: " & operation
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        operation = "+"
+        UpdateOperationLabel()
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        operation = "-"
+        UpdateOperationLabel()
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        operation = "*"
+        UpdateOperationLabel()
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        operation = "/"
+        UpdateOperationLabel()
+    End Sub
+
+    Private Sub btnPower_Click(sender As Object, e As EventArgs) Handles btnPower.Click
+        operation = "^"
+        UpdateOperationLabel()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim num1 As Double = Convert.ToDouble(txtNum1.Text)
+        Dim num2 As Double = Convert.ToDouble(txtNum2.Text)
+
+        Select Case operation
+            Case "+"
+                result = num1 + num2
+            Case "-"
+                result = num1 - num2
+            Case "*"
+                result = num1 * num2
+            Case "/"
+                If num2 <> 0 Then
+                    result = num1 / num2
+                Else
+                    lblResult.Text = "Cannot divide by zero!"
+                    Return
+                End If
+            Case "^"
+                result = Math.Pow(num1, num2)
+        End Select
+
+        lblResult.Text = "Result: " & result.ToString()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNum1.Clear()
+        txtNum2.Clear()
+        lblResult.Text = "Result:"
+        lblOperation.Text = "Operation: " ' Clear the operation display
+        operation = ""
+    End Sub
+End Class

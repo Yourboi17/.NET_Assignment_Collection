927b7ac1650793b3d46fc2b9dcdbbfc1279233bb
Raj Huidrom <129941875+Yourboi17@users.noreply.github.com>
2024-11-03 22:25:43 +0530

    Add files via upload

---
+++ Single and Multi-Level Inheritance/C#/SingleAndMultiLevelInheritanceC#/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating...");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking...");
+        }
+    }
+
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("Puppy is weeping...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started...\n");
+
+            Dog dog = new Dog();
+            Console.WriteLine("Single Inheritance Example:");
+            dog.Eat();
+            dog.Bark();
+
+            Console.WriteLine();
+
+            Puppy puppy = new Puppy();
+            Console.WriteLine("Multi-Level Inheritance Example:");
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Single and Multi-Level Inheritance/VB/SingleAndMultiLevelInheritanceVB/Module1.vb
@@ -0,0 +1,45 @@
+﻿Module InheritanceExample
+
+    Public Class Animal
+        Public Sub Eat()
+            Console.WriteLine("Animal is eating...")
+        End Sub
+    End Class
+
+    Public Class Dog
+        Inherits Animal
+
+        Public Sub Bark()
+            Console.WriteLine("Dog is barking...")
+        End Sub
+    End Class
+
+    Public Class Puppy
+        Inherits Dog
+
+        Public Sub Weep()
+            Console.WriteLine("Puppy is weeping...")
+        End Sub
+    End Class
+
+    Sub Main()
+        Console.WriteLine("Program started..." & vbNewLine)
+
+        Dim dog As New Dog()
+        Console.WriteLine("Single Inheritance Example:")
+        dog.Eat()
+        dog.Bark()
+
+        Console.WriteLine()
+
+        Dim puppy As New Puppy()
+        Console.WriteLine("Multi-Level Inheritance Example:")
+        puppy.Eat()
+        puppy.Bark()
+        puppy.Weep()
+
+        Console.WriteLine(vbNewLine & "Press Enter to exit...")
+        Console.ReadLine()
+    End Sub
+
+End Module
